3a0a6107835f84bb4f9143e2801960091446fb2f
Rômulo Victor <rbvictor@gmail.com>
2017-07-08 10:50:01 -0400

    set up express and react config

--- app.js
+++ app.js
@@ -18,12 +18,25 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+// React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// app.use('/', index);
+app.use('/', function(req, res) {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 app.use('/users', users);
+
+app.use('/api/suggestions', function (req, res) {
+  res.json({
+    "suggestions" : []
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
